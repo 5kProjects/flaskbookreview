a5457ef4e77ed5b84c8e025e2e1144a53da0cc76
birukbealy <belay.bura@gmail.com>
2021-04-19 23:47:03 +0300

    finished wpf form

---
+++ forms.py
@@ -0,0 +1,11 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, IntegerField, TextAreaField, SubmitField, PasswordField, SelectField
+
+from wtforms.validators import DataRequired, EqualTo, Length
+
+class BookForm(FlaskForm):
+    title = StringField('Title', validators=[DataRequired()])
+    author = StringField('Author', validators=[])
+    year = IntegerField('Year', validators=[])
+    isbn = StringField('Isbn', validators=[])
+    submit = SubmitField('Submit')
